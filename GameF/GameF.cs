5282d21b0e35bc256a8f0fca9369f3c0156b9620
Elzaaa <64545734+Elzaaa@users.noreply.github.com>
2021-03-17 01:03:30 +0700

    Добавлена логика игры

---
+++ GameF/GameF.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameF
+{
+    public class Game
+    {
+        int size;
+        public int moves { get; private set; }
+        public Game(int size)
+        {
+            this.size = size;
+        }
+        public void Start(int random = 0)
+        {
+
+        }
+        public int PressAt(int x, int y)
+        {
+            return 0;
+        }
+        public int GetDigitAt(int x, int y)
+        {
+            return 0;//пустое место
+        }
+        public bool Solved ()
+        {
+            return false;
+        }
+    }
+}
